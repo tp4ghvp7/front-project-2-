dafa80a2f3139edbef703a00cd81278ec12d0e73
Garrukzijian <u6128492@au.edu>
2022-02-19 00:14:12 +0700

    add login page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import logo from './logo.svg';
 import React,{Component} from 'react';
-import {Button,message} from 'antd';
+import {Button,message, Switch} from 'antd';
 import './App.css';
+import {BrowserRouter,HashRouter,Routes,Route}from "react-router-dom"
+//import page
+import Login from './pages/login/login';
+import Admin from './pages/admin/admin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{  
+  render(){
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/admin' element={<Admin/>}/>
+        </Routes> 
+      </BrowserRouter>
+    )
+  }
 }
-
 export default App;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from'./images/Logo.jpg'
+import './login.less'
+ 
+export default class Login extends Component {
+    render(){
+        return(
+            <div className='login'>
+                <div className='login-header'>
+                    {/* <img src={logo} alt="logo" /> */}
+                    <h1>React project: Web store backend</h1>
+                </div>
+                <div className='login-content'>
+                    <h1>User Login</h1>
+                    <div>Form page</div>
+                </div>   
+            </div>                
+        )
+    }
+}

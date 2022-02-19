608d5c1dc85ac3b4e3e8e10e05198cf3ecf06c48
Garrukzijian <u6128492@au.edu>
2022-02-19 13:37:58 +0700

    Use antd to add the login form

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { Form, Input, Button} from 'antd';
+import { UserOutlined,LockOutlined} from '@ant-design/icons';
+
 import logo from'./images/Logo.jpg'
 import './login.css'
+
+const Item = Form.Item
  
 export default class Login extends Component {
+    handleSubmit = e =>{
+        e.preventDefault()
+        alert('')
+    }
+
     render(){
         return(
             <div className='login'>
@@ -12,7 +22,24 @@ export default class Login extends Component {
                 </div>
                 <div className='login-content'>
                     <h1>User Login</h1>
-                    <div>Form page</div>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Item>
+                            <Input 
+                                prefix={<UserOutlined style={{color:'rgba(0,0,0,.25)'}}/>}
+                                placeholder="Username"
+                            />
+                        </Item>
+                        <Item>
+                            <Input 
+                                prefix={<LockOutlined type="lock"style={{color:'rgba(0,0,0,.25)'}}/>}
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Item>
+                        <Item>
+                            <Button type="primary" htmlType='submit' className='login-form-button'>log in</Button>
+                        </Item>
+                    </Form>
                 </div>   
             </div>                
         )
